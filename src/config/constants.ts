6591a5f4b03de30d828913feb8af2da51af0be77
NOPE finance <nope@solend.fi>
2021-09-25 14:33:02 -0700

    add devnet thingy

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -12,41 +12,49 @@ export const VERSION = process.env.NEXT_PUBLIC_VERSION
 export const IDO_STARTS = moment(process.env.NEXT_PUBLIC_IDO_START)
 
 export const RPC_ENDPOINTS: WalletEndpoint[] = [
-  {
-    id: 'parrot2',
-    network: 'mainnet-beta' as web3.Cluster,
-    rpcURL: 'https://lokidfxnwlabdq.main.genesysgo.net:8899/',
-    rpcName: 'GenesysGo RPC',
-    commitment: 'processed' as web3.Commitment,
-  },
-  {
-    id: 'parrot',
-    network: 'mainnet-beta' as web3.Cluster,
-    rpcURL: 'https://parrot.rpcpool.com',
-    rpcName: 'Triton RPC',
-    commitment: 'processed' as web3.Commitment,
-  },
-  {
-    id: 'serum',
-    network: 'mainnet-beta' as web3.Cluster,
-    rpcURL: 'https://solana-api.projectserum.com',
-    rpcName: 'Serum RPC',
-    commitment: 'processed' as web3.Commitment,
-  },
-  {
-    id: 'custom',
-    network: 'mainnet-beta' as web3.Cluster,
-    rpcURL: '',
-    rpcName: 'Custom RPC',
-    commitment: 'processed' as web3.Commitment,
-  },
   // {
-  //   id: 'devnet',
-  //   network: 'devnet' as web3.Cluster,
-  //   rpcURL: 'https://api.devnet.solana.com',
-  //   rpcName: 'Solana Devnet',
+  //   id: 'parrot2',
+  //   network: 'mainnet-beta' as web3.Cluster,
+  //   rpcURL: 'https://lokidfxnwlabdq.main.genesysgo.net:8899/',
+  //   rpcName: 'GenesysGo RPC',
   //   commitment: 'processed' as web3.Commitment,
   // },
+  // {
+  //   id: 'parrot',
+  //   network: 'mainnet-beta' as web3.Cluster,
+  //   rpcURL: 'https://parrot.rpcpool.com',
+  //   rpcName: 'Triton RPC',
+  //   commitment: 'processed' as web3.Commitment,
+  // },
+  // {
+  //   id: 'serum',
+  //   network: 'mainnet-beta' as web3.Cluster,
+  //   rpcURL: 'https://solana-api.projectserum.com',
+  //   rpcName: 'Serum RPC',
+  //   commitment: 'processed' as web3.Commitment,
+  // },
+  // {
+  //   id: 'custom',
+  //   network: 'mainnet-beta' as web3.Cluster,
+  //   rpcURL: '',
+  //   rpcName: 'Custom RPC',
+  //   commitment: 'processed' as web3.Commitment,
+  // },
+  {
+    id: 'devnet',
+    network: 'devnet' as web3.Cluster,
+    rpcURL: 'https://api.devnet.solana.com',
+    rpcName: 'Solana Devnet',
+    commitment: 'processed' as web3.Commitment,
+  },
+  {
+    id: 'devnet2',
+    network: 'devnet' as web3.Cluster,
+    rpcURL:
+      'https://wispy-white-bird.solana-devnet.quiknode.pro/77f628d0d9a021582e43e00e6b16cbbe19a08bf9/',
+    rpcName: 'Solana Devnet',
+    commitment: 'processed' as web3.Commitment,
+  },
 ]
 
 export const IDO_RESULTS = {
@@ -72,13 +80,10 @@ export const IDO_ENDPOINTS = [
   },
   {
     network: 'devnet' as web3.Cluster,
-    programId: '5s48HdiM1PjxqHDpGvZUVnX6eKbGbvN15rFHJ7RwxCv4',
-    usdcMint: 'G1Z261S3B2XQWCZo1qXJkEbeqkrcY1mVW3B3vMj5uqRq',
-    pools: [
-      '3ah8jT2jkHb3vhKGSxNK4Dm1XDY842vmJaTG8YaFtdyB',
-      '4qkLCR7JrkLCqfJ8iPKTBHGSsqvbY2EjVNeHx9iTdDSR',
-    ],
+    programId: 'A12dZpR9ZNwZM2Aa2aH6ArpUPKyHSpkJVdA76g2dEtX8',
+    usdcMint: '7XdWHLWy9Y71erdEfNdSpgKfigMqLLFeoVUSMwyorxZ4',
+    pools: ['7fHy45sUcxDzrLmiHJvk3oF5BtN36cCiWuqdYwFYVjbU'],
   },
 ]
 
-export const DEFAULT_RPC = RPC_ENDPOINTS.find((i) => i.id === 'parrot2')
+export const DEFAULT_RPC = RPC_ENDPOINTS.find((i) => i.id === 'devnet2')
